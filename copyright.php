09965adc1ad7bc20daf4ed7db798513e0c448640
swinkles92 <swinkles92@live.com>
2023-02-23 16:00:26 -0700

    Initial Commit
    Created the start page, copyright page, and images folder. Added 1 image (ishimura_logo.webp) to images folder. Added a link between the start page and copyright page.

---
+++ copyright.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<!-- This is displayed on the browser header -->
+		<title>USG Ishimura</title>
+		<style>
+		  body {
+		      background-color: cornflowerblue;
+		  }
+		  h1 {
+		      font-family: monospace;
+		      text-align: center;
+		  }
+		  .image1 {
+		      display: block;
+		      margin-left: auto;
+		      margin-right: auto;
+		      width: 300px;
+		      height: 300px;
+		  }
+		</style>
+	</head>
+	<!-- All HTML elements will go here -->
+	<body>
+		<h1>Copyright Info</h1>
+	</body>
+</html>
+<?php
+?>
---
+++ startPage.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<!-- This is displayed on the browser header -->
+		<title>USG Ishimura</title>
+		<style>
+		  body {
+		      background-color: cornflowerblue;
+		  }
+		  h1 {
+		      font-family: monospace;
+		      text-align: center;
+		  }
+		  .image1 {
+		      display: block;
+		      margin-left: auto;
+		      margin-right: auto;
+		      width: 300px;
+		      height: 300px;
+		  }
+		  .button {
+		      background-color: #e7e7e7; color: black;
+		      padding: 15px 32px;
+		      text-align: center;
+		      text-decoration: none;
+		      display: inline-block;
+		      font-size: 16px;
+		      border-radius: 4px;
+		      cursor: pointer;
+		  }
+		  .button:hover {
+		      background-color: white;
+		  }
+		  .buttonContainer {
+		      text-align: center;
+		  }
+		</style>
+	</head>
+	<!-- All HTML elements will go here -->
+	<body>
+		<img src="images/Ishimura_logo.webp" 
+			 alt="USG Ishimura Logo"
+			 class="image1">
+		<h1>Welcome to the USG Ishimura</h1>
+		<div class="buttonContainer">
+			<a href="copyright.php"
+		   	   class="button">
+		   	   Copyright Information</a>
+		</div>
+	</body>
+</html>
+<?php
+?>
